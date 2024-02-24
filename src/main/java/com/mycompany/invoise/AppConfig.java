e113165074ba03f930033da484db838adadebcc6
admin <jibacyrille@gmail.com>
2024-02-24 06:37:31 -0500

    Utilisation de applicationContext.xml co,,e fichier de config secondaire

--- src/main/java/com/mycompany/invoise/AppConfig.java
+++ src/main/java/com/mycompany/invoise/AppConfig.java
@@ -2,10 +2,12 @@ package com.mycompany.invoise;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@ComponentScan(basePackages = {"com.mycompany.invoise.controller.web","com.mycompany.invoise.service.prefix","com.mycompany.invoise.repository.database"})
-@PropertySource("classpath:application.properties")
+@ComponentScan(basePackages = {"com.mycompany.invoise.controller.web","com.mycompany.invoise.service.prefix","com.mycompany.invoise.repository.memory"})
+//@PropertySource("classpath:application.properties")
+@ImportResource("classpath:applicationContext.xml")
 public class AppConfig {
 }
